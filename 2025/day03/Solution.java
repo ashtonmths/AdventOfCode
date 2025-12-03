d19b81da27ac80977fbf2a8070c2b542b8c9f4e8
Subtilizer28 <mathiasashton07@gmail.com>
2025-12-04 00:28:19 +0530

    solve: day 3 part 1

--- 2025/day03/Solution.java
+++ 2025/day03/Solution.java
@@ -5,23 +5,38 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Solution {
-    
+
     public static void main(String[] args) throws IOException {
-        String input = Files.readString(Paths.get("2025/day03/input.txt")).trim();
-        
+        String input = Files.readString(Paths.get("input.txt")).trim();
+
         System.out.println("Part 1: " + part1(input));
         System.out.println("Part 2: " + part2(input));
     }
-    
-    private static String part1(String input) {
+
+    private static int part1(String input) {
         List<String> lines = Arrays.asList(input.split("\n"));
-        // TODO: Implement part 1
-        return "";
+        int totalSum = 0;
+        
+        for (String line : lines) {
+            int maxJoltage = 0;
+            for (int i = 0; i < line.length(); i++) {
+                for (int j = i + 1; j < line.length(); j++) {
+                    int digit1 = Integer.parseInt(String.valueOf(line.charAt(i)));
+                    int digit2 = Integer.parseInt(String.valueOf(line.charAt(j)));
+                    int joltage = digit1 * 10 + digit2;
+                    if (joltage > maxJoltage) {
+                        maxJoltage = joltage;
+                    }
+                }
+            }
+            totalSum += maxJoltage;
+        }
+        return totalSum;
     }
-    
-    private static String part2(String input) {
-        List<String> lines = Arrays.asList(input.split("\n"));
-        // TODO: Implement part 2
-        return "";
+
+    private static int part2(String input) {
+        // List<String> lines = Arrays.asList(input.split("\n"));
+
+        return 0;
     }
 }
